b9fd996a476cca5de25ccc765fc229acae5a80b7
Pheyi32 <feyisogoadeyemo.com>
2023-07-23 10:04:19 -0700

    Task 1 changes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,12 +16,10 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Displays 'HBNB!."""
+    """ Displays 'HBNB'"""
     return "HBNB"
 
 
 if __name__ == "__main__":
-	# Start the Flask development server
-	# Listen on all available network interfaces (0.0.0.0) and port 5000
 	app.run(host='0.0.0.0', port=5000)
 
